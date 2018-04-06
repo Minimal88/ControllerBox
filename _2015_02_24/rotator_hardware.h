5a025d60b6172e155040e06a34d5d41244965b2c
Unknown <tean88@gmail.com>
2018-04-05 18:48:41 -0600

    Revision de pines

---
+++ _2015_02_24/rotator_hardware.h
@@ -0,0 +1,9 @@
+/* rotator_hardware.h
+
+   Uncomment defines below if you're specifically using any of this hardware
+
+*/
+
+//#define HARDWARE_M0UPU
+//#define HARDWARE_EA4TX_ARS_USB    // if using EA4TX ARS USB hardware, customize rotator_features_e4tx_ars_usb.h (not rotator_features.h)
+//#define HARDWARE_WB6KCN    // customize rotator_features_wb6kcn.h, rotators_pins_wb6kcn.h, rotator_settings_wb6kcn.h
